4e0b0b09c9e33f82edfcf3afe508f2309629b2ca
jvispute15-sketch <jvispute15@gmail.com>
2025-11-13 00:15:32 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // The line below prints the string "Hello, World!" to the console
         System.out.println("Hello, World!");
+        System.out.println("Hello, World 2nd time !");
     }
 }
